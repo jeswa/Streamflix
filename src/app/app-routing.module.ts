d9eaab4dfeb766258382ee4b8c6bb63894211445
chagamareddy Rajeswari <crraji14@gmail.com>
2023-07-18 13:09:36 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 const routes: Routes = [
   { path: "login-form", component: LoginFormComponent },
   { path: "subscription-form", component: SubscriptionFormComponent },
-  { path: "movies-list", component: MoviesListComponent }
+  { path: "movies-list", component: MoviesListComponent },
+  { path: "movie-card", component: MovieCardComponent }
 ];
 
 @NgModule({
